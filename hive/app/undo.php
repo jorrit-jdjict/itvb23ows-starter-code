94d41ca16716c4851e376d9730c979d16475ab27
Jorrit de Jong <jorrit@jdjict.nl>
2024-01-17 12:59:38 +0100

    remove udno when no move has been made

--- hive/app/undo.php
+++ hive/app/undo.php
@@ -3,15 +3,18 @@ session_start(); // Start a session to manage game data.
 
 $db = include_once './database.php'; // Include the database connection.
 
-// Prepare a database query to retrieve the move information based on the last move ID.
-$stmt = $db->prepare('SELECT * FROM moves WHERE id = ' . $_SESSION['last_move']);
+if ($_SESSION['last_move'] > 0) {
+    // Prepare a database query to retrieve the move information based on the last move ID.
+    $stmt = $db->prepare('SELECT * FROM moves WHERE id = ' . $_SESSION['last_move']);
 
-$stmt->execute(); // Execute the database query.
+    $stmt->execute(); // Execute the database query.
 
-$result = $stmt->get_result()->fetch_array(); // Get the result of the query and fetch it as an array.
+    $result = $stmt->get_result()->fetch_array(); // Get the result of the query and fetch it as an array.
 
-$_SESSION['last_move'] = $result[5]; // Update the last move ID in the session.
+    $_SESSION['last_move'] = $result[5]; // Update the last move ID in the session.
 
-setState($result[6]); // Set the game state based on the retrieved state from the database.
+    setState($result[6]); // Set the game state based on the retrieved state from the database.
+
+}
 
 header('Location: ../index.php'); // Redirect to the main game page.
